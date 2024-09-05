b877a5d09eac7d89d8c29d8caaa5f06edc3d86b1
ShirinAziz <Shirin.aziz96@outlook.com>
2024-09-05 17:53:13 +0200

    #60 styla button

--- src/components/navbar/Button.jsx
+++ src/components/navbar/Button.jsx
@@ -1,8 +1,10 @@
+import styles from "./NavBar.module.css";
+
 const Button = ({ name = "Sign In" }) => {
   if (!name) {
     return <div>You need to Add title for this button</div>;
   }
-  return <button>{name}</button>;
+  return <button className={styles.btn}>{name}</button>;
 };
 
 export default Button;
