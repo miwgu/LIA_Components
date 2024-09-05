244085362697b1864f11150f7eb1ab7bf737186b
ShirinAziz <Shirin.aziz96@outlook.com>
2024-09-05 13:08:52 +0200

    #57 & # 59 Skadade side nav med hamburgermeny och la till länkar

--- src/components/navbar/SideNav.jsx
+++ src/components/navbar/SideNav.jsx
@@ -1,4 +1,28 @@
+import React, { useState } from "react";
+import styles from "./NavBar.module.css";
+
 const SideNav = () => {
-  return <>hamburgermeny</>;
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleSideNav = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div>
+      <button className={styles.navbarToggler} onClick={toggleSideNav}>
+        &#9776;
+      </button>
+      <nav className={`${styles.sidenav} ${isOpen ? styles.open : ""}`}>
+        <button className={styles.closeBtn} onClick={toggleSideNav}>
+          &times;
+        </button>
+        <a href="#home">Home</a>
+        <a href="#about">About</a>
+        <a href="#contact">Contact</a>
+      </nav>
+    </div>
+  );
 };
+
 export default SideNav;
