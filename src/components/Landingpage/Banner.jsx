f90be815e676d12c373f68e70d95ce7483c62e7a
JonasB91 <jonas.backas@hotmail.com>
2024-09-06 09:34:57 +0200

    Lägger till banner för landingpage/Home.jsx

---
+++ src/components/Landingpage/Banner.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Banner.module.css';
+
+const Banner = () => {
+  return (
+    <div className={styles.banner}>
+      <h1 className={styles.title}>Hitta din praktikplats hos oss!</h1>
+    </div>
+  );
+};
+
+export default Banner;
---
+++ src/components/Landingpage/Banner.stories.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Banner from './Banner';
+
+export default {
+  title: 'Landingpage/Banner',
+  component: Banner,
+};
+
+export const SimpleBanner = () => <Banner />;
