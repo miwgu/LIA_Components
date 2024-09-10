707732ce3a973e9222c2fe2b0cc396789741efc4
Miwa G <miwa.guhres@hotmail.com>
2024-09-10 12:55:21 +0200

    To connect front-end

---
+++ src/index.js
@@ -0,0 +1,6 @@
+export {default as Banner} from "./components/Landingpage/Banner";
+export {default as Login} from "./components/Login/Login";
+export {default as NavBar} from "./components/navbar/NavBar";
+export {default as ProfileEdit} from "./components/ProfileEdit/ProfileEdit";
+export {default as Register} from "./components/Register/Register";
+export {default as RegisterCompany} from "./components/RegisterCompany/RegisterCompany";
