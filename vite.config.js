fe37a4b9253ec1b1489dafc40fcd79c3290eb00d
Miwa G <miwa.guhres@hotmail.com>
2024-09-10 15:34:45 +0200

    Fix link

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'; 
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.js'), 
+      name: 'fwk-22-a-components',
+      formats: ['es', 'cjs'],
+      fileName: (format) => (format === 'es' ? 'index.esm.js' : 'index.js'),
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
+});
