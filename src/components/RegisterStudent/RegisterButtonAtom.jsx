f68365f664044e695e50b3cf605a131b615c44a5
Mikaela Johansson <mikaela.johansson87@gmail.com>
2024-09-24 17:09:00 +0200

    registrering student

--- src/components/RegisterStudent/RegisterButtonAtom.jsx
+++ src/components/RegisterStudent/RegisterButtonAtom.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import styles from './RegisterStudent.module.css'; 
 
-const RegisterButtonAtom = ({ title = "Register" }) => {
+const RegisterButtonAtom = ({ title = "Register", onClick }) => {
   if (!title) {
-    return <div>You need to Add title for this button</div>;
+    return <div>You need to add a title for this button</div>;
   }
-  return <button className={styles.regButton}>{title}</button>;
+  
+  return (
+    <button className={styles.regButton} onClick={onClick}>
+      {title}
+    </button>
+  );
 };
 
 export default RegisterButtonAtom;
+
