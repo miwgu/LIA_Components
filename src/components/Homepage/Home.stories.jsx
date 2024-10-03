aafd9630f19bbe48a4f12cecbc865050ab64d442
Realcapz <144321466+Realcapz@users.noreply.github.com>
2024-10-03 11:53:50 +0200

    Create Home.stories.jsx

---
+++ src/components/Homepage/Home.stories.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Home from './Home';
+
+export default {
+    title: 'LIA/Homepage/Home', 
+    component: Home,
+  };
+  
+
+export const Default = () => <Home />;
